226d2121567f95dd68d5146559a7f82d74c61173
NilsGrimbeck <nils@grimbeck.com>
2024-04-15 15:10:15 +0200

    run file

--- Energisystemprojekt/Our code/run.jl
+++ Energisystemprojekt/Our code/run.jl
@@ -1,6 +1,15 @@
 using Gurobi
+using JuMP
 
 include("model.jl")
 m, Capacity, Electricity = buildmodel("data.jl")
 print(m)
+
+set_optimizer_attribute(m, "LogLevel", 1)
+set_optimizer(m, Gurobi.Optimizer) #m is model from model file
+set_optimizer_attribute(m, "NumericFocus", 2)
+
+#optimizing
+optimize!(m)
+
 
