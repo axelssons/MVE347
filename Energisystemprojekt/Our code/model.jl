ffaec03fe024732238e74efed887c407b5765ab7
Drudzi <62624129+axelssons@users.noreply.github.com>
2024-04-15 16:13:15 +0200

    Update model.jl

--- Energisystemprojekt/Our code/model.jl
+++ Energisystemprojekt/Our code/model.jl
@@ -15,10 +15,18 @@ function buildmodel(data_file)
     )
 
     @constraint(m, waterlevel, WaterLevel[1] == WaterLevel[8760])
-    @constraint(m, capacity, Capacity[r, p] <= maxcaptable[r, p])
-    @constraint(m, load, Electricity[r, p, h] >= load[r, h])
-    @constraint(m, production, Electricity[r, p, h] <= Capacity[r, p] * cf[r, p, h])
-    @constraint(m, waterchange, WaterLevel[h+1] == WaterLevel[h] + hydro_inflow[h] - Electricity[:SE, :Hydro, h])
+    for r in REGION
+        for p in PLANT
+            @constraint(m, capacity, Capacity[r, p] <= maxcaptable[r, p])
+            for h in HOUR
+                @constraint(m, load, Electricity[r, p, h] >= load[r, h])
+                @constraint(m, production, Electricity[r, p, h] <= Capacity[r, p] * cf[r, p, h])
+            end
+        end
+    end
+    for h in 2:8759
+        @constraint(m, waterchange, WaterLevel[h+1] == WaterLevel[h] + hydro_inflow[h] - Electricity[:SE, :Hydro, h])
+    end
 
     return m, Capacity, Electricity
 end
